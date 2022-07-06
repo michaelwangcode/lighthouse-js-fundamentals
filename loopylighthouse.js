f0ea263410610223854af2618aa585cfb563b9ee
Michael Wang <michaelwangxv@gmail.com>
2022-07-06 18:48:33 +0000

    Commit new files

---
+++ loopylighthouse.js
@@ -0,0 +1,22 @@
+
+// Loopy Lighthouse Assignment
+
+for (let number = 100; number <= 200; number++)
+{
+  if (number % 3 === 0 && number % 4 === 0)
+  {
+    console.log("LoopyLighthouse");
+  }
+  else if (number % 3 === 0)
+  {
+    console.log("Loopy");
+  }
+  else if (number % 4 === 0)
+  {
+    console.log("Lighthouse");
+  }
+  else
+  {
+    console.log(number);
+  }
+}
