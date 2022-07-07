112dda0828aa4c2335dc312ef27eb15bcd1dfb39
Michael Wang <michaelwangxv@gmail.com>
2022-07-07 18:21:29 +0000

    Commit loopylighthouse2.js

---
+++ loopylighthouse2.js
@@ -0,0 +1,24 @@
+function loopyLighthouse(range, multiples, words)
+{
+  for (let number = range[0]; number <= range[1]; number++) 
+  {
+    if (number % multiples[0] === 0 && number % multiples[1] === 0) 
+    {
+      console.log(words[0] + words[1]);
+    }
+    else if (number % multiples[0] === 0) 
+    {
+      console.log(words[0]);
+    }
+    else if (number % multiples[1] === 0) 
+    {
+      console.log(words[1]);
+    }
+    else 
+    {
+      console.log(number);
+    }
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
