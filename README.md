b9e4ac75ac22edc5ee98dcde151f7fe51f8b1c05
michaelwangcode <michaelwangxv@gmail.com>
2022-10-28 12:11:07 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lighthouse JS Fundamentals
+
+## About
+
+This is a collection of JavaScript exercises completed during the Lighthouse Labs Prep Course (Section 4: Programming Fundamentals).
--- loopylighthouse2.js
+++ loopylighthouse2.js
@@ -1,24 +1,18 @@
-function loopyLighthouse(range, multiples, words)
-{
-  for (let number = range[0]; number <= range[1]; number++) 
-  {
-    if (number % multiples[0] === 0 && number % multiples[1] === 0) 
-    {
+// Loopy Lighthouse 2
+const loopyLighthouse = function(range, multiples, words) {
+
+  for (let number = range[0]; number <= range[1]; number++) {
+
+    if (number % multiples[0] === 0 && number % multiples[1] === 0) {
       console.log(words[0] + words[1]);
-    }
-    else if (number % multiples[0] === 0) 
-    {
+    } else if (number % multiples[0] === 0) {
       console.log(words[0]);
-    }
-    else if (number % multiples[1] === 0) 
-    {
+    } else if (number % multiples[1] === 0) {
       console.log(words[1]);
-    }
-    else 
-    {
+    } else {
       console.log(number);
     }
   }
-}
+};
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
